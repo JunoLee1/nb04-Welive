629545e3b1a778748f72c5612239ef72f7fa72f9
준오 <juno@jun-oui-noteubug.local>
2025-12-16 15:02:20 +0900

    토큰 제너레이터

---
+++ src/lib/tokens.ts
@@ -0,0 +1,21 @@
+import {
+  JWT_ACCESS_TOKEN_SECRET,
+  JWT_REFRESH_TOKEN_SECRET,
+}from './constants.ts';
+
+import jwt from 'jsonwebtoken'
+interface Tokens {
+  accessToken: string,
+  refreshToken: string,
+}
+
+export function generateToken( id : string): Tokens {
+  const accessToken = jwt.sign({sub: id}, JWT_ACCESS_TOKEN_SECRET, {
+    expiresIn:'10mins',
+  });
+
+  const refreshToken = jwt.sign({ sub:id }, JWT_REFRESH_TOKEN_SECRET,{
+    expiresIn:'1d',
+  });
+  return { accessToken, refreshToken };
+}
