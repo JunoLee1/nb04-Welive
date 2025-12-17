5f61e80244e314c9dc442f45f0593ebb71e782c4
준오 <juno@jun-oui-noteubug.local>
2025-12-17 10:39:35 +0900

    refactor: Initial login controller

--- src/features/auth/auth.controller.ts
+++ src/features/auth/auth.controller.ts
@@ -1,9 +1,19 @@
+import { RequestHandler } from "express";
+import { Service } from './auth.services.js'
+import * from'../DTO/auth.dto.js' //TODO: 로그인 request response dto 찾아서 넣기.
+
 export class Controller {
-    constructor() {}
+    constructor(
+      private readOnly service: Service
+    ) {}
 
-    async login() {}
+    loginHandler: RequestHandler = async (req, res, next) => {
+      const { username, password }  = req.body; //TODO: insert request response dto type
+      const data = this.service.login({ username, password });//TODO: insert types
+      res.status(200).send(data);
+    }
 
-    async logout() {}
+    logoutHandler: RequestHandler = async (req, res, next) => {}
     
-    async refreshToken() {}
+    refreshTokenHandler: RequestHandler = async (req, res, next) => {}
 }
