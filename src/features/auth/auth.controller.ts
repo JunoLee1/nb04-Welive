a395c146a06484a6a29feebccbae36982efdace8
준오 <juno@jun-oui-noteubug.local>
2025-12-17 11:38:34 +0900

    feat: added request type in controller

--- src/features/auth/auth.controller.ts
+++ src/features/auth/auth.controller.ts
@@ -1,14 +1,14 @@
 import { RequestHandler } from "express";
 import { Service } from './auth.services.js'
-import * from'../DTO/auth.dto.js' //TODO: 로그인 request response dto 찾아서 넣기.
+import { LoginRequestDTO }from'../DTO/auth.dto.js' 
 
 export class Controller {
     constructor(
-      private readOnly service: Service
+      private readonly service: Service,
     ) {}
 
     loginHandler: RequestHandler = async (req, res, next) => {
-      const { username, password }  = req.body; //TODO: insert request response dto type
+      const { username, password }: LoginRequestDTO = req.body; //TODO: insert request response dto type
       const data = this.service.login({ username, password });//TODO: insert types
       res.status(200).send(data);
     }
