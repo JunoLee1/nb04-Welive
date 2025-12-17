bbcc835ad95963c37f663e94c7f6091132633b7b
준오 <juno@jun-oui-noteubug.local>
2025-12-17 18:25:05 +0900

    refactor: validate params

---
+++ src/lib/middleware/validator.ts
@@ -0,0 +1,16 @@
+import type { RequestHandler } from 'express';
+import { z } from 'zod';
+
+export function validateParams <T extends z.ZodTypeAny> (schema:T): RequestHandler{
+    return  (req, res, next) => { 
+        const result = schema.safeParse(req.params)
+        if (!result.success) {
+            return res.status(400).json({
+                message: '잘못된 요청 입니다.',
+                error: result.error.issues
+            })
+        }
+    }
+    
+}
+
