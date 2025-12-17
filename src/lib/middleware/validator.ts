6cfa7330a4e750a02665dab7a403fabcf29589d3
준오 <juno@jun-oui-noteubug.local>
2025-12-17 18:28:38 +0900

    refactor: created validator for body

--- src/lib/middleware/validator.ts
+++ src/lib/middleware/validator.ts
@@ -10,7 +10,20 @@ export function validateParams <T extends z.ZodTypeAny> (schema:T): RequestHandl
                 error: result.error.issues
             })
         }
+        next()
     }
     
 }
 
+export function validateBody<T extends z.ZodTypeAny> (schema:T): RequestHandler{
+    return (req, res, next) => {
+        const result = schema.safeParse(req.body)
+        if (!result.success) {
+            return res.status(400).json({
+                message: '잘못된 요청 입니다.',
+                error: result.error.issues
+            })
+        }
+        next()
+    }
+}
