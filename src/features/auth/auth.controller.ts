a5119b8f48547fbfad6a15c5812227ea58fa1b60
준오 <juno@jun-oui-noteubug.local>
2025-12-18 13:29:16 +0900

    refactor(auth): fix login/logout handlers and normalize HTTP responses.

--- src/features/auth/auth.controller.ts
+++ src/features/auth/auth.controller.ts
@@ -9,11 +9,21 @@ export class Controller {
 
     loginHandler: RequestHandler = async (req, res, next) => {
       const { username, password }: LoginRequestDTO = req.body; 
-      const data = this.service.login({ username, password });
-      res.status(200).send(data);
+      const data = await this.service.login({ username, password });
+      return res.status(200).send(data);
     }
 
-    logoutHandler: RequestHandler = async (req, res, next) => {}
+    logoutHandler: RequestHandler = async (req, res, next) => {
+      // TODO: logout logic
+      // logout success ? delete cookie, return 204 status
+      this.service.clearTokenCookies(res)
+      return res.status(204)
+    }
     
-    refreshTokenHandler: RequestHandler = async (req, res, next) => {}
+    refreshTokenHandler: RequestHandler = async (req, res, next) => {
+      // TODO: refresh token logic
+      // TODO: should I check cookies type ?
+      // renew token  sucucess, set token to cookie, return 204 status
+      return res.status(204)
+    }
 }
