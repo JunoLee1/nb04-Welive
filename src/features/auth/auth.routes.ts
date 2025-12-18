b8c48f55e429181a41b40a2f39230590f275701b
준오 <juno@jun-oui-noteubug.local>
2025-12-18 15:29:15 +0900

    chore(auth) : import authenticate from passport to auth

--- src/features/auth/auth.routes.ts
+++ src/features/auth/auth.routes.ts
@@ -4,13 +4,13 @@ import type { Express }from "express"
 import { Service } from "./auth.services.js";
 import { validate } from "../../lib/middleware/validator.js";
 import { loginSchema } from "./auth.validator.js";
-
-// 로그인 라우트
+import { passport } from "../../lib/passport/index.js"
 const router = Router();
 const service = new Service();
 const controller = new Controller(service);
 
-router.post("/login", validate(loginSchema, "body"), controller.loginHandler);
+//로그인 API
+router.post("/login",passport.authenticate('local', { session: false }), validate(loginSchema, "body"), controller.loginHandler);
 // 로그아웃 라우트
 router.post("/logout", controller.logoutHandler); 
 // 토큰 갱신 라우트
