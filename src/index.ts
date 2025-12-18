072eafeec58509ef548dc54a1c5a2cf5b68a9703
준오 <juno@jun-oui-noteubug.local>
2025-12-18 11:42:22 +0900

    fix(router): adjust error middleware in main router

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import  express from "express";
 import type {Express, RequestHandler, ErrorRequestHandler} from "express";
 import cookieParser from "cookie-parser";
 import type { Passport } from "passport";
+import { errorHandler } from "./lib/middleware/error.middleware/error-handler.js";
 declare const passport: typeof Passport ; 
 
 export const app: Express = require("express")();
@@ -9,7 +10,8 @@ export const app: Express = require("express")();
 app.use(express.json());
 app.use(cookieParser() as RequestHandler );
 app.use(express.urlencoded({ extended: false }));
-app.use("/api/v2")
+app.use("/api/v2");
+app.use(errorHandler as ErrorRequestHandler);
 //app.use(passport.initialize());
 //app.use(passport.session());
 
