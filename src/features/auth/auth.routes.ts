b3a567be53dc35588da7717c10603550cc1617f3
준오 <juno@jun-oui-noteubug.local>
2025-12-18 09:53:42 +0900

    chore(auth): use shared validator.

--- src/features/auth/auth.routes.ts
+++ src/features/auth/auth.routes.ts
@@ -2,12 +2,15 @@ import { Router } from "express";
 import { Controller } from "./auth.controller.js";
 import type { Express }from "express"
 import { Service } from "./auth.services.js";
+import { validate } from "../../lib/middleware/validator.js";
+import { loginSchema } from "./auth.validator.js";
+
 // 로그인 라우트
 const router = Router();
 const service = new Service();
 const controller = new Controller(service);
 
-router.post("/login",controller.loginHandler); //TODO: insert validator middleawre
+router.post("/login", validate(loginSchema, "body"), controller.loginHandler);
 // 로그아웃 라우트
 router.post("/logout", controller.logoutHandler); 
 // 토큰 갱신 라우트
