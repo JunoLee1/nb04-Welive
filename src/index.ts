db5792a4a6ed63adb1fdb90b4c4c4f54fb7c1774
준오 <juno@jun-oui-noteubug.local>
2025-12-19 11:26:27 +0900

    fix(./index): cjs -> esm, add variable const app = express:Express = require() => const app = express.Router(); app.use("path") => app.use(path,parameter)

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 import  express from "express";
-import type {Express, RequestHandler, ErrorRequestHandler} from "express";
+import type { RequestHandler, ErrorRequestHandler} from "express";
 import cookieParser from "cookie-parser";
-import type { Passport } from "passport";
+import passport from "passport";
 import { errorHandler } from "./lib/middleware/error.middleware/error-handler.js";
-declare const passport: typeof Passport ; 
-
-export const app: Express = require("express")();
+import apiRouter from './features/index.js'
+const app = express.Router()
 
 app.use(express.json());
 app.use(cookieParser() as RequestHandler );
 app.use(express.urlencoded({ extended: false }));
-app.use("/api/v2");
+app.use("/api/v2", apiRouter);
 app.use(errorHandler as ErrorRequestHandler);
-//app.use(passport.initialize());
-//app.use(passport.session());
+app.use(passport.initialize());
+export default app;
+
 
