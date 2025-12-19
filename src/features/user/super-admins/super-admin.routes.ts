f891f273091f14dfe257569a4951ff7ab4efdc26
준오 <juno@jun-oui-noteubug.local>
2025-12-19 17:40:47 +0900

    chore: formatting

--- src/features/user/super-admins/super-admin.routes.ts
+++ src/features/user/super-admins/super-admin.routes.ts
@@ -1,17 +1,16 @@
-import { Router } from "express"
-import  { superAdminSignUpSchema }from "./super-admin.validator.js"
-import { Controller } from './super-admin.controller.js'
+import { Router } from "express";
+import { superAdminSignUpSchema } from "./super-admin.validator.js";
+import { Controller } from "./super-admin.controller.js";
 import { validate } from "../../../lib/middleware/validator.js";
-
 
 // create super admin post API
 const router = Router();
 const controller = new Controller();
 
-router.post('/super-admins',
-    validate(superAdminSignUpSchema, "body"),
-    controller.signUpHandler
-)
+router.post(
+  "/super-admins",
+  validate(superAdminSignUpSchema, "body"),
+  controller.signUpHandler
+);
 
-   
-export default router
+export default router;
