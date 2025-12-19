440b589d0ed3fec859a4b492dd6a5dc710bb68a2
준오 <juno@jun-oui-noteubug.local>
2025-12-19 10:28:58 +0900

    feat(lib/passport): adjust local-strategy

--- src/lib/passport/index.ts
+++ src/lib/passport/index.ts
@@ -1,6 +1,8 @@
 import passport from 'passport'
 import { accessTokenStrategy, refreshTokenStrategy } from './jwt-strategy.js';
+import { localStrategy } from './local-strategy.js'
 
+passport.use('local', localStrategy);
 passport.use('access-token', accessTokenStrategy);
 passport.use('refresh-token', refreshTokenStrategy);
 
