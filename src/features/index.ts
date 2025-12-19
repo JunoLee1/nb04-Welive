c24bff083cdb9969e1b7483ec56f62b6983809dc
준오 <juno@jun-oui-noteubug.local>
2025-12-19 17:34:03 +0900

    feat: connect to users router

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 import { authRoutes }from './auth/auth.routes.js';
-
+import usersRoutes from './user/user.routes.js'
 const router = express.Router();
 
 router.use(authRoutes);
-//router.use(postRouter);
+router.use(usersRoutes);
 
 export default router;
