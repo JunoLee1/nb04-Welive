c833f3e96bf49a0d8c16d6ce372a2720c635533c
준오 <juno@jun-oui-noteubug.local>
2025-12-19 13:20:35 +0900

    feat(user) : frame for users Api

---
+++ src/features/user/index.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+const router = Router();
+
+/**
+ * API user feature entry router
+ *  api/V2/users/ 
+ - super admins
+ - admins
+ - residents
+ - me
+ */
+// router.use("/super-admins",superAdminsApi);
+// router.use("/admins",adminsApi);
+// router.use("/residents",residentsApi);
+// router.use("/me",userInfoApi);
+
+export default router
