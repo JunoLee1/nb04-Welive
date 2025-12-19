629797598b136170200bdfdedc4de3bf17d93a43
준오 <juno@jun-oui-noteubug.local>
2025-12-19 17:55:53 +0900

    feat(admin/routes): frame for admin api

---
+++ src/features/user/admins/admins.routes.ts
@@ -0,0 +1,29 @@
+import { Router } from 'express';
+const router = Router();
+
+
+// create admin Api
+// address : users/admins
+
+
+// access admins API
+// address : users/admins
+
+
+// patch the admins Join status API
+// address : users/admins/joinStatus
+
+
+// patch an admin Join status API
+// address : users/admins/{id}/joinStatus
+
+// Patch an admin user info
+// address : users/admins/{id}
+
+// delete admin account API
+// address : users/admins/{id}
+
+
+// delete rejected Admin user
+// address : users/admins/
+
