1455dbf2a7720faf55ddec39582b88c88935fcc6
준오 <juno@jun-oui-noteubug.local>
2025-12-19 14:34:58 +0900

    feat(user/super-adimin): super admin zod

---
+++ src/features/user/superAdmins/super-admin.validator.ts
@@ -0,0 +1,12 @@
+import {z} from 'zod'
+
+const superAdminSignUpSchema = z.object({
+    email: z.email(),
+    password:z.string(),//TODO: use regex for mix character and number
+    username: z.string(),// TODO: check regulation for username from front, User Identification 
+    contact: z.string(),//TODO: contain "-" 
+    name: z.string(),
+    isActive:z.boolean()// default false
+})
+
+export type SuperAdminSignUpSchema = z.infer<typeof superAdminSignUpSchema>;
