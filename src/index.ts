3c08f6aca044ce4c6f6d30dc1800ae10450d8805
준오 <juno@jun-oui-noteubug.local>
2025-12-19 13:04:39 +0900

    feat: added port

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,9 @@ import cookieParser from "cookie-parser";
 import passport from "passport";
 import { errorHandler } from "./lib/middleware/error.middleware/error-handler.js";
 import apiRouter from './features/index.js'
-const app = express.Router()
+
+const app = express()
+const PORT = process.env.PORT || "3000"
 
 app.use(express.json());
 app.use(cookieParser() as RequestHandler );
@@ -12,6 +14,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/v2", apiRouter);
 app.use(errorHandler as ErrorRequestHandler);
 app.use(passport.initialize());
+app.listen(PORT,() => {
+    console.log(`Server is ruunnig ${PORT}`)
+})
 export default app;
 
 
