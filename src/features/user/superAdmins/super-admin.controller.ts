cb9fe063d4356b91ea7fa35bac1983d0e6702c6b
준오 <juno@jun-oui-noteubug.local>
2025-12-19 16:20:36 +0900

    chore(super/controller):added logs and connected to service

--- src/features/user/superAdmins/super-admin.controller.ts
+++ src/features/user/superAdmins/super-admin.controller.ts
@@ -1,14 +1,15 @@
-import { RequestHandler } from "express";
-import { SuperAdminCreateReqDTO } from "./super-admin.dto";
-//import {Service} from "./super-admin.controller"
-
-//const service = new Service()
+import type { RequestHandler } from "express";
+import type { SuperAdminCreateReqDTO } from "./super-admin.dto.js";
+import { Service } from "./super-admin.service.js";
+import { Repository } from "./super-admin.repository.js";
+const service = new Service(new Repository());
 export class Controller {
-    signUpHandler:RequestHandler = async (req, res, next) => {
-        console.log("received from routes")
-        const {email, password, name, username, contact} = req.body// validated value from validator
-        //service.signUpHandler({email, password, name, username, contact}: SuperAdminCreateReqDTO );
-        console.log("✅ result")
-        return res.status(200);
-    }
+  signUpHandler: RequestHandler = async (req, res, next) => {
+    console.log("received from routes");
+    const { email, password, name, username, contact } =
+      req.body as SuperAdminCreateReqDTO; // validated value from validator
+    service.signUpHandler({ email, password, name, username, contact });
+    console.log("✅ result");
+    return res.status(200);
+  };
 }
