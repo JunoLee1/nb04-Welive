aa1a611b547577171621eb70246df439eb03efbf
준오 <juno@jun-oui-noteubug.local>
2025-12-20 14:47:19 +0900

    refactor(admins/routes): admin routes
    add Todo comment

--- src/features/user/admins/admins.routes.ts
+++ src/features/user/admins/admins.routes.ts
@@ -1,29 +1,39 @@
 import { Router } from 'express';
+import { Controller } from "./admins.controller.js";
+import { id } from './id/admin-id.routes.js'
+const controller = new Controller();
 const router = Router();
 
-
+router.use(`/${id}`,id)
 // create admin Api
 // address : users/admins
-
+router.post("/",
+    //TODO: add validator
+    controller.register
+)
 
 // access admins API
 // address : users/admins
-
+router.get("/",
+    //TODO: check the user is super admin
+    //TODO: add validator
+    controller.accessList
+)
 
 // patch the admins Join status API
 // address : users/admins/joinStatus
-
-
-// patch an admin Join status API
-// address : users/admins/{id}/joinStatus
-
-// Patch an admin user info
-// address : users/admins/{id}
-
-// delete admin account API
-// address : users/admins/{id}
+router.patch("/joinStatus",
+    //TODO: check the user is super admin
+    //TODO: add validator
+    controller.modifyStatus
+)
 
 
 // delete rejected Admin user
 // address : users/admins/
-
+router.delete('/',
+    //TODO: check the user is super admin
+    //TODO: add validator
+    controller.deleteAdmins
+)
+export default router;
