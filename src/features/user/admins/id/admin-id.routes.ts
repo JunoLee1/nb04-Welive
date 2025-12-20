73ef7b2bbd6dc7ccc55c933519140afacb33a1cc
준오 <juno@jun-oui-noteubug.local>
2025-12-20 13:47:30 +0900

    feat: seperate id directory

---
+++ src/features/user/admins/id/admin-id.routes.ts
@@ -0,0 +1,9 @@
+
+// patch an admin Join status API
+// address : users/admins/{id}/joinStatus
+
+// Patch an admin user info
+// address : users/admins/{id}
+
+// delete admin account API
+// address : users/admins/{id}
