3349cf542be292812e319aa51133f40da5961973
준오 <juno@jun-oui-noteubug.local>
2025-12-21 13:04:19 +0900

    feat(admins/validation): implement body schema

--- src/features/user/admins/admins.validation.ts
+++ src/features/user/admins/admins.validation.ts
@@ -1,5 +1,11 @@
 import {z} from 'zod';
  
+const requestBodySchema = z.object({
+    username: z.string(),
+    email: z.email(),
+    contact:z.string(),
+    password:z.string(),
+})
 const requestQuerySchema = z.object({
     page: z.number(),
     limit: z.number(),
@@ -8,3 +14,4 @@ const requestQuerySchema = z.object({
 })
 
 export type ReqQuerySchema = z.infer<typeof requestQuerySchema>
+export type BodySchema = z.infer<typeof requestBodySchema>
