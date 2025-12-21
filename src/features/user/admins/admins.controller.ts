3dc61a0acd5c68f25b29a38941601ff6e9fde13b
준오 <juno@jun-oui-noteubug.local>
2025-12-21 17:27:11 +0900

    feat(admin/controller): connect to the service

--- src/features/user/admins/admins.controller.ts
+++ src/features/user/admins/admins.controller.ts
@@ -1,8 +1,17 @@
-import type {RequestHandler} from 'express'
+import type { RequestHandler } from 'express';
+import { Service } from './admins.service.js';
+import type { RequestBody } from './admin.dto.js';
+import { Repository } from './admins.repository.js';
+
+const repo =  new Repository()
+const service = new Service( repo);
 export class Controller {
     constructor(){};
 
-    register: RequestHandler = (req, res, next) => {}
+    register: RequestHandler = async(req, res, next) => {
+        const { email, name, username, password, avatar, contact} :RequestBody = req.body;
+        await service.register({ email, name, username, password, avatar, contact })
+    }
     accessList:RequestHandler = (req, res, next) => {}
     modifyStatus:RequestHandler = (req, res, next) => {}
     deleteAdmins:RequestHandler = (req, res, next) => {}
