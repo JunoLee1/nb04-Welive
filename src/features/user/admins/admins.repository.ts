a48182802deca95465e0aec5e2fcf6dcadb25584
준오 <juno@jun-oui-noteubug.local>
2025-12-21 17:19:34 +0900

    feat(admins/repo): create admin user

--- src/features/user/admins/admins.repository.ts
+++ src/features/user/admins/admins.repository.ts
@@ -1,6 +1,5 @@
-import type { RequestHandler } from "express";
 import prisma from "../../../lib/prisma.js"
-
+import type { RequestBody } from './admin.dto.js';
 export class Repository {
     constructor(){};
     /*
@@ -16,17 +15,63 @@ export class Repository {
         const userId = await prisma.user.findUnique({
             where:{
                 id,
-            }
+            },
         })
         return userId;
     }
-
+    findByUsername = async(username: string) => {
+        const userName = await prisma.user.findUnique({
+            where:{ username },
+        });
+        return userName
+     }
     findByEmail = async(email: string) => {
         const userEmail = await prisma.user.findUnique({
             where:{
                 email
+            },
+        })
+        return userEmail;
+    }
+    createAdmin = async( input: RequestBody ) => {
+        const newAdmin = await prisma.user.create({
+            data:{
+                email:input.email ?? "",
+                username:input.username ??"",
+                name: input.name ?? "",
+                password: input.password ?? "",
+                contact: input.contact ?? "",
+                role: "ADMIN" ,
+                avatar: input.avatar ?? ""
+            },
+            select:{
+                id:true,
+                email:true,
+                username:true,
+                name:true,
+                contact:true,
+                role:true,
+                isActive: true,
+                hasNext: true,
+                avatar:true,
+                adminOf:{
+                    select:{
+                        id: true,
+                        name: true,
+                        createdAt: true,
+                        updatedAt:true,
+                        address: true,
+                        description: true,
+                        buildingNumberFrom: true,
+                        buildingNumberTo: true,
+                        floorCountPerBuilding: true,
+                        unitCountPerFloor: true,
+                        officeNumber: true,
+                        adminId: true,
+                    },
+                }
             }
         })
-        return userEmail
+        return newAdmin;
     }
 }
