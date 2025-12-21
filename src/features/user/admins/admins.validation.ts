36dda79dba50f46a350ecb34deece65fa90bc116
준오 <juno@jun-oui-noteubug.local>
2025-12-21 13:20:52 +0900

    feat(admins/validation): implement joined status

--- src/features/user/admins/admins.validation.ts
+++ src/features/user/admins/admins.validation.ts
@@ -1,17 +1,26 @@
 import {z} from 'zod';
  
-const requestBodySchema = z.object({
+export const requestBodySchema = z.object({
     username: z.string(),
     email: z.email(),
     contact:z.string(),
     password:z.string(),
-})
-const requestQuerySchema = z.object({
+});
+
+export const requestQuerySchema = z.object({
     page: z.number(),
     limit: z.number(),
     searchKeyword: z.string(),
     joinStatus: z.string(),
-})
+});
 
-export type ReqQuerySchema = z.infer<typeof requestQuerySchema>
-export type BodySchema = z.infer<typeof requestBodySchema>
+export const joinStatusSchema = z.object({
+    joinStatus: z.string(),
+});
+
+// ------ infer the schema
+export type ReqQuerySchema = z.infer<typeof requestQuerySchema>;
+
+export type BodySchema = z.infer<typeof requestBodySchema>;
+
+export type JoinStatusSchema = z.infer<typeof joinStatusSchema>;
