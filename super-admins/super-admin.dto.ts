691ff30fde47fe548d72a2524cd5c46861812fe3
준오 <juno@jun-oui-noteubug.local>
2025-12-21 13:37:30 +0900

    fix: restoring

---
+++ super-admins/super-admin.dto.ts
@@ -0,0 +1,20 @@
+type JoinStatus ="PENDING" | "APPROVED"| "REJECTED";
+type Role = "SUPER_ADMIN" | "ADMIN"| "USER";
+export interface SuperAdminCreateReqDTO {
+    email:string,
+    password: string,
+    name: string,
+    username:string,
+    contact: string
+}
+
+export interface SuperAdminCreateResDTO {
+    email:string,
+    name: string,
+    username:string,
+    contact: string,
+    role: Role,
+    avatar: string | null,
+    joinStatus: JoinStatus,
+    isActive: Boolean,
+}
