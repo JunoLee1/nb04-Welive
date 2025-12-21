292e3ffd9d3a198a08c975349a51f3e92c4719c7
준오 <juno@jun-oui-noteubug.local>
2025-12-21 11:55:55 +0900

    feat(index): added cors, refactored import request

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import  express from "express";
-import type { RequestHandler, ErrorRequestHandler} from "express";
+import express , { type RequestHandler, type ErrorRequestHandler }from "express";
+import cors from 'cors';
 import cookieParser from "cookie-parser";
 import passport from "passport";
 import { errorHandler } from "./lib/middleware/error.middleware/error-handler.js";
@@ -11,6 +11,10 @@ const PORT = process.env.PORT || "3000"
 app.use(express.json());
 app.use(cookieParser() as RequestHandler );
 app.use(express.urlencoded({ extended: false }));
+app.use(cors({
+    origin:"http://localhost:9000",
+    credentials: true
+}))
 app.use("/api/v2", apiRouter);
 app.use(errorHandler as ErrorRequestHandler);
 app.use(passport.initialize());
