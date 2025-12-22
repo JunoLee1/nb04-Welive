52dd68e36af6555df321e11b7d7574de6bd55f19
준오 <juno@jun-oui-noteubug.local>
2025-12-22 18:59:34 +0900

    chore: temp comment

--- src/features/user/admins/admins.controller.ts
+++ src/features/user/admins/admins.controller.ts
@@ -1,42 +1,70 @@
-import type { RequestHandler } from 'express';
-import { Service } from './admins.service.js';
-import type { RequestBody, ReqParams, } from './admin.dto.js';
-import { Repository } from './admins.repository.js';
-import { HttpError } from '../../../lib/middleware/error.middleware/httpError.js'
+import type { RequestHandler } from "express";
+import { Service } from "./admins.service.js";
+import type { RequestBody, ReqParams } from "./admin.dto.js";
+import { Repository } from "./admins.repository.js";
+import { HttpError } from "../../../lib/middleware/error.middleware/httpError.js";
 import { JoinStatus } from "../../../../prisma/generated/client.js";
 
-const repo =  new Repository()
-const service = new Service( repo );
+const repo = new Repository();
+const service = new Service(repo);
 export class Controller {
-    constructor(){};
+  constructor() {}
 
-    register: RequestHandler = async(req, res, next) => {
-        const { email, name, username, password, avatar, contact} :RequestBody = req.body;
-        await service.register({ email, name, username, password, avatar, contact })
-        return res.status(204).json()
-    };
+  register: RequestHandler = async (req, res, next) => {
+    const { email, name, username, password, avatar, contact }: RequestBody =
+      req.body;
+    await service.register({
+      email,
+      name,
+      username,
+      password,
+      avatar,
+      contact,
+    });
+    return res.status(204).json();
+  };
 
-    accessList:RequestHandler = async (req, res, next) => {
-        console.log("received request from router");
-        const { page, limit, searchKeyword, joinStatus } = req.query;
-        const user = req.user;
-        if (! user) throw new HttpError(401, "인증되지 않은 유저 입니다")//TODO 에러 메시지 수정
-        if ( user.role !== "SUPER_ADMIN") throw new HttpError(403, "권한이 없습니다")
-        console.log(user.role);
-        console.log("📧 connect to the service");
-        
-        const pageNumber = Number(page) ?? 1
-        const limitNumber = Number(limit) ?? 10
-        const status = (joinStatus ?? "PENDING") as JoinStatus;
-        const keyword = searchKeyword as string
-        const result = await service.accessList({ pageNumber, limitNumber, keyword, joinStatus:status }) 
-        console.log("📧 return from service ", result)
-        return res.status(204).json({
-            data:result
-        })
-    };
+  accessList: RequestHandler = async (req, res, next) => {
+    console.log("received request from router");
+    const { page, limit, searchKeyword, joinStatus } = req.query;
+    const user = req.user;
+    if (!user) throw new HttpError(401, "인증되지 않은 유저 입니다"); //TODO 에러 메시지 수정
+    if (user.role !== "SUPER_ADMIN")
+      throw new HttpError(403, "권한이 없습니다");
+    console.log(user.role);
+    console.log("📧 connect to the service");
 
-    modifyStatus:RequestHandler = (req, res, next) => {}
-    deleteAdmins:RequestHandler = (req, res, next) => {}
+    const pageNumber = Number(page) ?? 1;
+    const limitNumber = Number(limit) ?? 10;
+    const status = (joinStatus ?? "PENDING") as JoinStatus;
+    const keyword = searchKeyword as string;
+    const result = await service.accessList({
+      pageNumber,
+      limitNumber,
+      keyword,
+      joinStatus: status,
+    });
+    console.log("📧 return from service ", result);
+    return res.status(204).json({
+      data: result,
+    });
+  };
 
+  modifyStatus: RequestHandler = async (req, res, next) => {
+    const { joinStatus } = req.body;
+    const user = req.user;
+    if (!user) throw new HttpError(401, "인증되지 않은 유저 입니다");
+    if (user.role !== "SUPER_ADMIN")
+      throw new HttpError(403, "권한이 없습니다");
+    //await service.modifyStatus(joinStatus);
+    return res.status(204).json();
+  };
+
+  deleteAdmins: RequestHandler = async (req, res, next) => {
+    const user = req.user;
+    if (!user) throw new HttpError(401, "인증되지 않은 유저 입니다");
+    if (user.role !== "SUPER_ADMIN")
+      throw new HttpError(403, "권한이 없습니다");
+    //await service.deleteAdmins();
+  };
 }
