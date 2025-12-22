0f2afb263c5c278ce603f07cf56d215f54f1919c
준오 <juno@jun-oui-noteubug.local>
2025-12-22 18:25:14 +0900

    feat(admins/repo): findMany to access adamin list

--- src/features/user/admins/admins.repository.ts
+++ src/features/user/admins/admins.repository.ts
@@ -1,77 +1,119 @@
-import prisma from "../../../lib/prisma.js"
-import type { RequestBody } from './admin.dto.js';
+import prisma from "../../../lib/prisma.js";
+import type { RequestBody } from "./admin.dto.js";
+import { JoinStatus } from "../../../../prisma/generated/client.js";
+
+type Param = {
+  whereCondition: object;
+  skip: number;
+  limitNumber: number;
+  status: JoinStatus | null;
+};
+
 export class Repository {
-    constructor(){};
-    /*
-    findMany = async() => {
-        const result = await prisma.user.findMany({
-            where:{
-                
-            }
-        })
-    }
-    */
-    findById = async (id: string) => {
-        const userId = await prisma.user.findUnique({
-            where:{
-                id,
-            },
-        })
-        return userId;
-    }
-    findByUsername = async(username: string) => {
-        const userName = await prisma.user.findUnique({
-            where:{ username },
-        });
-        return userName
-     }
-    findByEmail = async(email: string) => {
-        const userEmail = await prisma.user.findUnique({
-            where:{
-                email
-            },
-        })
-        return userEmail;
-    }
-    createAdmin = async( input: RequestBody ) => {
-        const newAdmin = await prisma.user.create({
-            data:{
-                email:input.email ?? "",
-                username:input.username ??"",
-                name: input.name ?? "",
-                password: input.password ?? "",
-                contact: input.contact ?? "",
-                role: "ADMIN" ,
-                avatar: input.avatar ?? ""
-            },
-            select:{
-                id:true,
-                email:true,
-                username:true,
-                name:true,
-                contact:true,
-                role:true,
-                isActive: true,
-                hasNext: true,
-                avatar:true,
-                adminOf:{
-                    select:{
-                        id: true,
-                        name: true,
-                        createdAt: true,
-                        updatedAt:true,
-                        address: true,
-                        description: true,
-                        buildingNumberFrom: true,
-                        buildingNumberTo: true,
-                        floorCountPerBuilding: true,
-                        unitCountPerFloor: true,
-                        officeNumber: true,
-                        adminId: true,
-                    },
-                }
-            }
-        })
-        return newAdmin;
-    }
+  constructor() {}
+
+  findMany = async ({ whereCondition, skip, limitNumber, status }: Param) => {
+    const result = await prisma.user.findMany({
+      where: {
+        ...whereCondition,
+        joinStatus: status as JoinStatus,
+      },
+      select: {
+        id: true,
+        name: true,
+        contact: true,
+        email: true,
+        username: true,
+        avatar: true,
+        isActive: true,
+        hasNext: true,
+        adminOf: {
+          select: {
+            id: true,
+            createdAt: true,
+            updatedAt: true,
+            name: true,
+            address: true,
+            description: true,
+            officeNumber: true,
+            buildingNumberFrom: true,
+            buildingNumberTo: true,
+            floorCountPerBuilding: true,
+            unitCountPerFloor: true,
+            adminId: true,
+          },
+        },
+      },
+      take: limitNumber,
+      skip,
+    });
+    return result;
+  };
+
+  findById = async (id: string) => {
+    const userId = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+    return userId;
+  };
+  findByUsername = async (username: string) => {
+    const userName = await prisma.user.findUnique({
+      where: { username },
+    });
+    return userName;
+  };
+  findByEmail = async (email: string) => {
+    const userEmail = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    return userEmail;
+  };
+  createAdmin = async (input: RequestBody) => {
+    const newAdmin = await prisma.user.create({
+      data: {
+        email: input.email ?? "",
+        username: input.username ?? "",
+        name: input.name ?? "",
+        password: input.password ?? "",
+        contact: input.contact ?? "",
+        role: "ADMIN",
+        avatar: input.avatar ?? "",
+        approvedAt: "",
+      },
+      select: {
+        id: true,
+        email: true,
+        username: true,
+        name: true,
+        contact: true,
+        role: true,
+        isActive: true,
+        approvedAt: true,
+        createdAt: true,
+        hasNext: true,
+        avatar: true,
+        adminOf: {
+          select: {
+            id: true,
+            name: true,
+            createdAt: true,
+            updatedAt: true,
+            address: true,
+            description: true,
+            buildingNumberFrom: true,
+            buildingNumberTo: true,
+            floorCountPerBuilding: true,
+            unitCountPerFloor: true,
+            officeNumber: true,
+            adminId: true,
+          },
+        },
+      },
+    });
+    return newAdmin;
+  };
 }
