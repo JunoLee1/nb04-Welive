7b9b2c445efe70ec3fac22ad4d409366bcb305da
준오 <juno@jun-oui-noteubug.local>
2025-12-22 09:30:42 +0900

    chore(auth/val): fix useranme and password minimum length

--- src/features/auth/auth.validator.ts
+++ src/features/auth/auth.validator.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-  username: z.string().min(3), //TODO: check username from frontend requirements
-  password: z.string().min(6), //TODO: check password from frontend requirements
+  username: z.string().min(5), 
+  password: z.string().min(8),
 });
 
 export const refreshTokenSchema = z.object({
