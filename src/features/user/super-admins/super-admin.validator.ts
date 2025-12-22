9fd7ecd8e262b2e77f6f01f12ca20b450d2f9c27
준오 <juno@jun-oui-noteubug.local>
2025-12-22 09:34:05 +0900

    chore(super-admin/val): add minimum length for username and password.

--- src/features/user/super-admins/super-admin.validator.ts
+++ src/features/user/super-admins/super-admin.validator.ts
@@ -2,9 +2,9 @@ import {z} from 'zod'
 
 export const superAdminSignUpSchema = z.object({
     email: z.email(),
-    password:z.string(),//TODO: use regex for mix character and number
-    username: z.string(),// TODO: check regulation for username from front, User Identification 
-    contact: z.string(),//TODO: contain "-" 
+    password:z.string().min(8),
+    username: z.string().min(5),
+    contact: z.string(),
     name: z.string(),
     isActive:z.boolean()// default false
 })
