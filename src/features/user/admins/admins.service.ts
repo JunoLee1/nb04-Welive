96950e530f90a8582474ea9dc07941e75a212d52
준오 <juno@jun-oui-noteubug.local>
2025-12-22 09:23:22 +0900

    chore(admins/service): add TODO Comment and  log

--- src/features/user/admins/admins.service.ts
+++ src/features/user/admins/admins.service.ts
@@ -8,17 +8,18 @@ export class Service {
 
     register = async(input: RequestBody):Promise<AdminsCreateResponseDTO> => {
         const { email, name, username, password, avatar } = input;
-
+        console.log("⚓️receivded:", email, name, username, password, avatar);
         const duplicatedEmail = await this.repo.findByEmail(email);
         if( duplicatedEmail ) throw new  HttpError(400, "해당 이메일은 이미 존재하는 이메일 입니다.");//TODO:fix error message
         const duplicatedUsername = await this.repo.findByUsername( username );
         if( duplicatedUsername ) throw new  HttpError(400, "해당 이메일은 이미 존재하는 유저 아이디 입니다.");
         
         const newAdmin = this.repo.createAdmin(input);
-
+        //TODO: 토큰 생성
         const result = {
             ...newAdmin
         }
+        console.log("✌️result: ",result)
         return result;
     }
 }
