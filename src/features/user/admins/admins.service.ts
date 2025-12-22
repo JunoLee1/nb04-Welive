1f996046eb02190cbee4e4d8506ae9ab92624520
준오 <juno@jun-oui-noteubug.local>
2025-12-22 18:18:58 +0900

    feat(admins/serv): accessList
    * pagenation
    * access single object

--- src/features/user/admins/admins.service.ts
+++ src/features/user/admins/admins.service.ts
@@ -1,25 +1,85 @@
-import { HttpError } from '../../../lib/middleware/error.middleware/httpError.js';
-import type { RequestBody, AdminsCreateResponseDTO} from './admin.dto.js';
-import  {Repository} from './admins.repository.js'
+import { HttpError } from "../../../lib/middleware/error.middleware/httpError.js";
+import type {
+  RequestBody,
+  AdminsCreateResponseDTO,
+  ReqParams,
+  AccessListOfAdminsResDTO,
+} from "./admin.dto.js";
+import { Repository } from "./admins.repository.js";
 export class Service {
-    constructor(
-       readonly repo: Repository
-    ){}
+  constructor(readonly repo: Repository) {}
 
-    register = async(input: RequestBody):Promise<AdminsCreateResponseDTO> => {
-        const { email, name, username, password, avatar } = input;
-        console.log("⚓️receivded:", email, name, username, password, avatar);
-        const duplicatedEmail = await this.repo.findByEmail(email);
-        if( duplicatedEmail ) throw new  HttpError(400, "해당 이메일은 이미 존재하는 이메일 입니다.");//TODO:fix error message
-        const duplicatedUsername = await this.repo.findByUsername( username );
-        if( duplicatedUsername ) throw new  HttpError(400, "해당 이메일은 이미 존재하는 유저 아이디 입니다.");
-        
-        const newAdmin = this.repo.createAdmin(input);
-        //TODO: 토큰 생성
-        const result = {
-            ...newAdmin
+  register = async (input: RequestBody): Promise<AdminsCreateResponseDTO> => {
+    const { email, name, username, password, avatar } = input;
+    console.log("⚓️receivded:", email, name, username, password, avatar);
+    const duplicatedEmail = await this.repo.findByEmail(email);
+    if (duplicatedEmail)
+      throw new HttpError(400, "해당 이메일은 이미 존재하는 이메일 입니다."); //TODO:fix error message
+    const duplicatedUsername = await this.repo.findByUsername(username);
+    if (duplicatedUsername)
+      throw new HttpError(
+        400,
+        "해당 이메일은 이미 존재하는 유저 아이디 입니다."
+      );
+
+    const newAdmin = this.repo.createAdmin(input);
+    const result = {
+      ...newAdmin,
+    };
+    console.log("✌️result: ", result);
+    return result;
+  };
+  accessList = async (input: ReqParams): Promise<AccessListOfAdminsResDTO> => {
+    console.log("received request from access list routes");
+    const { pageNumber, limitNumber, keyword, joinStatus } = input;
+    const skip = (pageNumber - 1) * limitNumber;
+    console.log(pageNumber, limitNumber, skip);
+
+    const whereCondition = keyword
+      ? {
+          OR: [
+            { username: { contains: keyword } },
+            { email: { contains: keyword } },
+            { contact: { contains: keyword } },
+            { name: { contains: keyword } },
+          ], //TODO: Refactor
         }
-        console.log("✌️result: ",result)
-        return result;
-    }
+      : {};
+    const admins = await this.repo.findMany({
+      limitNumber,
+      skip,
+      whereCondition,
+      status: joinStatus,
+    });
+    const data = admins.map((u) => ({
+      id: u.id,
+      contact: u.contact,
+      name: u.name,
+      role: "ADMIN",
+      avatar: u.avatar,
+      isActive: u.isActive,
+      approvedAt: null,
+      adminOf: u.adminOf
+        ? {
+            id: u.adminOf.id,
+            name: u.adminOf.name,
+            createdAt: u.adminOf.createdAt,
+            updatedAt: u.adminOf.updatedAt,
+            address: u.adminOf.address,
+            description: u.adminOf.description,
+            officeNumber: u.adminOf.officeNumber,
+            buildingNumberFrom: u.adminOf.buildingNumberFrom,
+            buildingNumberTo: u.adminOf.buildingNumberTo,
+            floorCountPerBuilding: u.adminOf.floorCountPerBuilding,
+            unitCountPerFloor: u.adminOf?.unitCountPerFloor,
+            adminId: u.adminOf?.adminId,
+          }
+        : null,
+    }));
+
+    return {
+      data,
+      hasNext: true,
+    };
+  };
 }
