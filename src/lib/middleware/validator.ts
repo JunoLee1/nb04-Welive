441b583c97c8ffe5d4ae1d0ee3809dddcde5b46e
준오 <juno@jun-oui-noteubug.local>
2025-12-22 09:20:24 +0900

    chore(mid): add temp log

--- src/lib/middleware/validator.ts
+++ src/lib/middleware/validator.ts
@@ -1,9 +1,14 @@
 import type { RequestHandler } from 'express';
 import { z } from 'zod';
 
-export function validateParams <T extends z.ZodTypeAny> (schema:T): RequestHandler{
-    return  (req, res, next) => { 
-        const result = schema.safeParse(req.params)
+type Target = 'params' | 'body' | 'query';
+
+export const validate = <T extends z.ZodTypeAny>(schema:T, target:Target):RequestHandler => {
+    return (req, res, next) => {
+        const dataToValidate = req[target];
+        console.log("☑️:",req[target]); // test 동안 지우지 말것
+        const result = schema.safeParse(dataToValidate);
+        console.log("☑️ result :",result) // test 동안 지우지 말것
         if (!result.success) {
             return res.status(400).json({
                 message: '잘못된 요청 입니다.',
