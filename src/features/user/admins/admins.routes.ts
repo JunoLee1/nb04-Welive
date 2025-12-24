6a0e7f063bffa36ad58c56a86c2e01aaa4a935a8
준오 <juno@jun-oui-noteubug.local>
2025-12-24 14:54:20 +0900

    chore(admins/routes): formatting routes

--- src/features/user/admins/admins.routes.ts
+++ src/features/user/admins/admins.routes.ts
@@ -1,42 +1,40 @@
-import { Router } from 'express';
+import { Router } from "express";
 import { Controller } from "./admins.controller.js";
-import  idRouter  from './id/admin-id.routes.js';
-import { validate } from '../../../lib/middleware/validator.js';
-import  { requestBodySchema, requestQuerySchema, joinStatusSchema } from './admins.validation.js';
-import passport from "../../../lib/passport/index.js"
+import idRouter from "./id/admin-id.routes.js";
+import { validate } from "../../../lib/middleware/validator.js";
+import {
+  requestBodySchema,
+  requestQuerySchema,
+  joinStatusSchema,
+} from "./admins.validation.js";
+import passport from "../../../lib/passport/index.js";
 const controller = new Controller();
-const router = Router();
+const adminRouter = Router();
 
-router.use('/id', idRouter)
+adminRouter.use('/id', idRouter)
 // create admin Api
 // address : users/admins
-router.post("/",
-    validate( requestBodySchema, 'body'),
-    controller.register
-)
+adminRouter.post("/", validate(requestBodySchema, "body"), controller.register);
 
 // access admins API
 // address : users/admins
-router.get("/",
-    validate(requestQuerySchema,'query'),
-    passport.authenticate('accessToken', { session: false }),
-    controller.accessList
-)
+adminRouter.get(
+  "/",
+  validate(requestQuerySchema, "query"),
+  passport.authenticate("accessToken", { session: false }),
+  controller.accessList
+);
 
 // patch the admins Join status API
 // address : users/admins/joinStatus
-router.patch("/joinStatus",
-    //TODO: check the user is super admin
-    validate(joinStatusSchema,'body'),
-    passport.authenticate('accessToken', { session: false }),
-    controller.modifyStatus
-)
-
+adminRouter.patch(
+  "/joinStatus",
+  validate(joinStatusSchema, "body"),
+  passport.authenticate("accessToken", { session: false }),
+  controller.modifyStatus
+);
 
 // delete rejected Admin user
 // address : users/admins/
-router.delete('/',
-    //TODO: check the user is super admin
-    controller.deleteAdmins
-)
-export default router;
+adminRouter.delete("/", controller.deleteRejectedAdmins);
+export default adminRouter;
