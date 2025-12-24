4fb22c9c148be00c33437c7116342c85ae93c766
준오 <juno@jun-oui-noteubug.local>
2025-12-24 14:46:04 +0900

    fix(auth/jwt): ?? -> if -else

---
+++ src/lib/passport/jwt-strategy.ts
@@ -0,0 +1,46 @@
+import type { StrategyOptions, VerifiedCallback } from 'passport-jwt';
+import { Strategy as JwtStrategy } from 'passport-jwt';
+import {
+  ACCESS_TOKEN_COOKIE_NAME,
+  JWT_ACCESS_TOKEN_SECRET,
+  JWT_REFRESH_TOKEN_SECRET,
+  REFRESH_TOKEN_COOKIE_NAME,
+} from '../constants.js';
+import prisma from '../prisma.js';
+
+interface JwtPayload {
+  sub: string,
+  iat?: number,
+  exp?: number
+}
+
+const accessTokenOptions: StrategyOptions = {
+  jwtFromRequest: (req) => req.cookies[ACCESS_TOKEN_COOKIE_NAME],
+  secretOrKey: JWT_ACCESS_TOKEN_SECRET,
+}
+const refreshTokenOptions: StrategyOptions = {
+  jwtFromRequest: (req) => req.cookies[REFRESH_TOKEN_COOKIE_NAME],
+  secretOrKey: JWT_REFRESH_TOKEN_SECRET,
+};
+
+console.log("accessTokenOptions:",accessTokenOptions)
+async function jwtVerify( payload:JwtPayload, done: VerifiedCallback ) {
+  const user = await prisma.user.findUnique({
+    where: { id : payload.sub }
+  })
+  if (!user) {
+  return done(null, false);
+}
+ return done(null, user);
+};
+
+export const accessTokenStrategy = new JwtStrategy(
+  accessTokenOptions,
+  jwtVerify
+);
+
+export const refreshTokenStrategy = new JwtStrategy(
+  refreshTokenOptions,
+  jwtVerify
+);
+
