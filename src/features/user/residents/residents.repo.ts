eebc79dc2937cd5db285c44ce0ace9ce2fcca092
준오 <juno@jun-oui-noteubug.local>
2025-12-26 18:03:02 +0900

    feat(resident/repo): create findMany repo

--- src/features/user/residents/residents.repo.ts
+++ src/features/user/residents/residents.repo.ts
@@ -1,7 +1,13 @@
 import { HttpError } from "../../../lib/middleware/error.middleware/httpError.js";
 import prisma from "../../../lib/prisma.js";
-import type { ResidentCreateResponse } from "./residents.dto.js";
-import type { ResidentCreateSchema } from "./residents.validator.js";
+import type {
+  ResidentCreateResponse,
+  ResidentUserFindAllPageResponse,
+} from "./residents.dto.js";
+import type {
+  ResidentCreateSchema,
+  ReqParamQuerySchema,
+} from "./residents.validator.js";
 
 type FindKey = "id" | "username" | "contact" | "email";
 type StatusAction = "APPROVED" | "REJECTED";
@@ -18,55 +24,63 @@ const buildWhereClause = (field: FindKey, value: string) => {
       return { email: value };
   }
 };
+type Query = {
+  page: number;
+  limit: number;
+  keyword: object;
+};
 
 export class Repository {
-    constructor (){}
-  register = async (input:ResidentCreateSchema): Promise<ResidentCreateResponse> => {
-    const {email, username, name, contact, password } = input;
+  constructor() {}
+  register = async (
+    input: ResidentCreateSchema
+  ): Promise<ResidentCreateResponse> => {
+    const { email, username, name, contact, password } = input;
     const dulplicatedEmail = await this.findOne("email", email);
-    const duplicatedContact = await this.findOne("contact", contact)
-    const duplicatedUsername= await this.findOne("username", username)
-   
-    if(dulplicatedEmail) throw new HttpError(400,"해당 이메일은 이미 존재 합니다")
-    if(duplicatedContact) throw new HttpError(400,"해당 휴대폰 번호는 이미 존재 합니다")
-    if(duplicatedUsername) throw new HttpError(400,"해당 아이디는 이미 존재 합니다")
+    const duplicatedContact = await this.findOne("contact", contact);
+    const duplicatedUsername = await this.findOne("username", username);
+
+    if (dulplicatedEmail)
+      throw new HttpError(400, "해당 이메일은 이미 존재 합니다");
+    if (duplicatedContact)
+      throw new HttpError(400, "해당 휴대폰 번호는 이미 존재 합니다");
+    if (duplicatedUsername)
+      throw new HttpError(400, "해당 아이디는 이미 존재 합니다");
     const newResident = await prisma.user.create({
-      data:{
+      data: {
         email,
         username,
         role: "USER",
         name,
         contact,
         password,
-        joinStatus:"PENDING",
+        joinStatus: "PENDING",
       },
-      select:{
+      select: {
         email: true,
-        username:true,
+        username: true,
         role: true,
-        name:true,
-        contact:true,
-        password:true,
-        joinStatus:true,
+        name: true,
+        contact: true,
+        password: true,
+        joinStatus: true,
         isActive: true,
-        createdAt:true,
-        updatedAt:true,
-        resident:{
-          select:{
+        createdAt: true,
+        updatedAt: true,
+        resident: {
+          select: {
             id: true,
-            apartmentId:true,
+            apartmentId: true,
             unit: true,
-            building:true,
+            building: true,
             isHouseholder: true,
-            createdAt:true,
-            updatedAt:true,
-          }
-        }
-      }
-      
-
-    })
-    return newResident
+            createdAt: true,
+            updatedAt: true,
+          },
+        },
+      },
+    });
+    return newResident;
   };
   findOne = async (field: FindKey, value: string) => {
     const where = buildWhereClause(field, value);
@@ -75,16 +89,18 @@ export class Repository {
     });
     return resident;
   };
-  findMany = async (field: FindKey, value: string) => {
-    const where = buildWhereClause(field, value);
+  findMany = async ({ keyword }: Query) => {
     const residents = await prisma.user.findMany({
-      where,
-      //.. querry params
+      where: {
+        ...keyword,
+      },
+      include: {
+        resident: true,
+      },
     });
     return residents;
   };
   modifyResidentStatus = async (id: string, joinStatus: StatusAction) => {};
   modifyResidentsStatus = async (joinStatus: StatusAction) => {};
-  delete = async (field: FindKey, value: string) => {
-  };
+  delete = async (field: FindKey, value: string) => {};
 }
