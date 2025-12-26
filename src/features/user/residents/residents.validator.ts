7685be6f33781b8e7b9e8fefbd7bf05e69eeff53
준오 <juno@jun-oui-noteubug.local>
2025-12-26 15:48:59 +0900

    feat(user/resident/validator): zod for create query schema

---
+++ src/features/user/residents/residents.validator.ts
@@ -0,0 +1,25 @@
+import { z } from "zod";
+
+export const residentCreateSchema = z.object({
+  email: z.email(),
+  password: z.string(),
+  avatar: z.string().optional(),
+  username: z.string(),
+  name: z.string(),
+  contact: z.string(),
+  resident: z.object({
+    apartmentId: z.string(),
+    building: z.number().int(),
+    unit: z.number().int(),
+  }),
+});
+
+export const reqParamQuerySchema = z.object({
+  page: z.coerce.number(),
+  limit: z.coerce.number(),
+  unit: z.coerce.number(),
+  building: z.coerce.number(),
+});
+
+export type ResidentCreateSchema = z.infer<typeof residentCreateSchema>;
+export type ReqParamQuerySchema = z.infer<typeof reqParamQuerySchema>;
