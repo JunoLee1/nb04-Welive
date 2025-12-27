98e58f684e6d6a664798d7c02d8964e49da0270f
준오 <juno@jun-oui-noteubug.local>
2025-12-27 14:51:00 +0900

    feat(resident/controller): implement modify status

--- src/features/user/residents/residents.controller.ts
+++ src/features/user/residents/residents.controller.ts
@@ -3,6 +3,8 @@ import { Service } from "./residents.service.js";
 import type {
   ResidentCreateSchema,
   ReqParamQuerySchema,
+  JoinStatusSchema,
+  ParamSchema
 } from "./residents.validator.ts";
 import { Repository } from "./residents.repo.js";
 import { HttpError } from "../../../lib/middleware/error.middleware/httpError.js";
@@ -43,14 +45,21 @@ export class Controller {
   };
 
   modifyResidentsStatus: RequestHandler = async (req, res, next) => {
-    await service.modifyResidentsStatus;
+    const { joinStatus } = req.body;
+    const result = await service.modifyResidentsStatus(joinStatus);
+     if (!result) throw new HttpError(500, "알수없는 에러 입니다");
+    return res.status(204).json();
   };
 
   modifyResidentStatus: RequestHandler = async (req, res, next) => {
-    await service.modifyResidentStatus;
+    const { id } = req.params as ParamSchema ;
+    const { joinStatus } = req.body;
+    const result = await service.modifyResidentStatus(id, joinStatus);
+    if (!result) throw new HttpError(500, "알수없는 에러 입니다");
+    return res.status(204).json();
   };
 
   delete: RequestHandler = async (req, res, next) => {
-    await service.delete;
+    await service.deleteMany;
   };
 }
