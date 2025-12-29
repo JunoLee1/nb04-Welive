0922b35940a7b5820df666a5d9caf48fd4d8472f
준오 <juno@jun-oui-noteubug.local>
2025-12-29 18:57:27 +0900

    chore: add error message

--- src/lib/middleware/error.middleware/httpError.ts
+++ src/lib/middleware/error.middleware/httpError.ts
@@ -1,8 +1,11 @@
-export class HttpError {
-    status: number;
-  constructor(status: number) {
+export class HttpError extends Error {
+  status: number;
+  message: string;
+  constructor(status: number, message: string) {
     {
+      super(message);
       this.status = status;
+      this.message = message;
     }
   }
 }
