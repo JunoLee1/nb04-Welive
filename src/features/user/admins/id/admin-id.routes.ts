5883b3bedbec1ce7a7bcf188641fe4c67abc82f4
준오 <juno@jun-oui-noteubug.local>
2025-12-29 14:19:58 +0900

    chore(): formatting

--- src/features/user/admins/id/admin-id.routes.ts
+++ src/features/user/admins/id/admin-id.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { Controller } from "./admin-id.controller.js";
 import { validate } from "../../../../lib/middleware/validator.js";
-import { joinStatusSchema,requestBodySchema } from "../admins.validation.js";
+import { joinStatusSchema, requestBodySchema } from "../admins.validation.js";
 
 const idRouter = Router();
 const controller = new Controller();
@@ -12,7 +12,7 @@ idRouter.patch(
   "/joinStatus",
   //TODO:TEST
 
-  validate(joinStatusSchema,"body"),
+  validate(joinStatusSchema, "body"),
   controller.modifyJoinStatus
 );
 
@@ -21,14 +21,13 @@ idRouter.patch(
 idRouter.patch(
   "/",
   //TODO:TEST
-  validate(requestBodySchema,"body"),
+  validate(requestBodySchema, "body"),
   controller.modifyUserInfo
 );
 
 // delete admin account API
 // address : users/admins/{id}
-  //TODO:TEST
 idRouter.delete("/", controller.deleteAdmin);
-  //TODO:TEST
+//TODO:TEST
 
 export default idRouter;
