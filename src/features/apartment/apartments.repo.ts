2d24807095238bd55b1f98b6a8f74739395afc24
준오 <juno@jun-oui-noteubug.local>
2025-12-29 14:01:14 +0900

    feat(apt/repo): mapping searchKeyword, findUnique Id
    create Object for searchKeyword that type is SearchKeword.
    
    {key, value} => {name: string}

--- src/features/apartment/apartments.repo.ts
+++ src/features/apartment/apartments.repo.ts
@@ -1,3 +1,32 @@
-export class Repository {
+import type { RequestQuery } from "./apartments.dto.js";
+import prisma from "../../lib/prisma.js";
 
+export class Repository {
+  findMany = async ({ searchKeyword }: RequestQuery) => {
+    const where =
+      searchKeyword && Object.entries(searchKeyword).length > 0
+        ? {
+            OR: Object.entries(searchKeyword).map(([key, value]) => ({
+              [key]: {
+                contains: value,
+                mode: "strict",
+              },
+            })),
+          }
+        : {};
+    const apts = await prisma.apartment.findMany({
+      where
+    });
+    return apts
+  };
+
+  findOne = async ( id: string ) => {
+    const apt = await prisma.apartment.findUnique({
+        where: {
+            id
+        },
+
+    })
+    return apt;
+  };
 }
