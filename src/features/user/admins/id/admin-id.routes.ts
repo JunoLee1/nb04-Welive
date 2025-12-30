b6d7f32a8567905052b64155387fbbd9a4da2bff
준오 <juno@jun-oui-noteubug.local>
2025-12-30 16:11:14 +0900

    chore: requestPatchSchema <= requestBodySchema

--- src/features/user/admins/id/admin-id.routes.ts
+++ src/features/user/admins/id/admin-id.routes.ts
@@ -1,33 +1,29 @@
 import { Router } from "express";
 import { Controller } from "./admin-id.controller.js";
 import { validate } from "../../../../lib/middleware/validator.js";
-import { joinStatusSchema, requestBodySchema } from "../admins.validation.js";
+import { joinStatusSchema, requestPatchSchema } from "../admins.validation.js";
 
 const idRouter = Router();
 const controller = new Controller();
 
 // patch an admin Join status API
-// address : users/admins/{id}/joinStatus
+// address : users/admins/:{id}/joinStatus
 idRouter.patch(
   "/joinStatus",
-  //TODO:TEST
-
   validate(joinStatusSchema, "body"),
   controller.modifyJoinStatus
 );
 
 // Patch an admin user info
-// address : users/admins/{id}
+// address : users/admins/:{id}
 idRouter.patch(
   "/",
-  //TODO:TEST
-  validate(requestBodySchema, "body"),
+  validate(requestPatchSchema, "body"),
   controller.modifyUserInfo
 );
 
 // delete admin account API
 // address : users/admins/{id}
 idRouter.delete("/", controller.deleteAdmin);
-//TODO:TEST
 
 export default idRouter;
