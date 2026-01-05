50faf91e4498d3e63cbad235a7387fc16ddbbea7
준오 <juno@jun-oui-noteubug.local>
2026-01-05 13:33:02 +0900

    feat: implement upload

--- src/features/user/me/me.routes.ts
+++ src/features/user/me/me.routes.ts
@@ -3,12 +3,14 @@ import { Controller } from "./me.controller.js";
 import { validate } from "../../../lib/middleware/validator.js";
 import { avatarImageSchema, passwordSchema } from "./me.validator.js";
 import passport from "../../../lib/passport/index.js";
+import upload from "../../../lib/middleware/upload.js";
 
 const meRouter = Router();
 const controller = new Controller();
 meRouter.patch(
   "/avatar",
   passport.authenticate("accessToken", { session: false }),
+  upload.single("avatarImage"),
   validate(avatarImageSchema, "body"),
   controller.modifiedAvatar
 ); // TODO:test
