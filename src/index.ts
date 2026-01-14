dc8449186ffa9fb3f05956067f821f4c8cccec67
준오 <juno@jun-oui-noteubug.local>
2026-01-14 18:06:01 +0900

    add origin address

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser() as RequestHandler );
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin:"http://localhost:3000", 
+    origin:"https://weliv.netlify.app", 
     credentials: true
 }));
 app.use(passport.initialize());
