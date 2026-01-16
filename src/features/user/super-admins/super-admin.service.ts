9e264d955f52e4a2455d6a06df457173f79908a4
준오 <juno@jun-oui-noteubug.local>
2026-01-16 09:35:11 +0900

    chore: unused import and console

--- src/features/user/super-admins/super-admin.service.ts
+++ src/features/user/super-admins/super-admin.service.ts
@@ -4,7 +4,6 @@ import type {
   SuperAdminCreateResDTO,
 } from "./super-admin.dto.js";
 import { Repository } from "./super-admin.repository.js";
-import prisma from "../../../lib/prisma.js";
 import bcrypt from "bcrypt"
 export class Service {
   constructor(readonly repository: Repository) {}
@@ -17,17 +16,12 @@ export class Service {
     contact,
     avatar
   }: SuperAdminCreateReqDTO):Promise<SuperAdminCreateResDTO>=> {
-    console.log("⚓️ received from controller:",email, password, name, contact, username, avatar)
    const userEmail = await this.repository.findUniqueEmail(email);
-    console.log("found email:", userEmail);
    if(userEmail) throw new HttpError(400, "잘못된 요청 입니다.");
     const userPhoneNumber = await this.repository.findUniquePhoneNumber(contact);
     if(userPhoneNumber) throw new HttpError(400, "잘못된 요청 입니다.");
-    console.log("found phone number:", userPhoneNumber);
-    const userName = await this.repository.findUniqueUsername(username);
-    //console.log("found username:", userName);
+    const userName = await this.repository.findUniqueUsername(username,"SUPER_ADMIN");;
     if(userName)  throw new HttpError(400, "잘못된 요청 입니다.");
-    console.log("✅ finished prunning");
     const hashedPassword = await bcrypt.hash(password, 10)
     const data = await this.repository.createSuperAdmin({
         email:userEmail,
